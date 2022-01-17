f3220acb7a164dbc22097c6a6634fbd0a14a415e
username <nomadicalli@mail.ru>
2022-01-17 22:18:39 +0300

    ДОбавил два новых свойства

--- script.js
+++ script.js
@@ -21,12 +21,37 @@ let phone = {
         
     },
 
-    
+    switch() {
+        this.power = !this.power
+    }
+
+
+
+
 }
 
 phone.batteryPower = {
     charge: 40,
 }
+
+phone.outgoingMessage = [
+    {
+        to: "89386854942",
+        time: "23:00",
+        message: "Hi",
+    }
+];
+
+phone.incoming = [
+    {
+        from: "89899052345",
+        time: "01:00",
+        message: "Д1авала",
+    }
+]
+
+
+
 
 
 
